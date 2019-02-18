37e5a77088d9a869a57f34d80890102b28a0c33c
Nick Schneider <nickschneider101@gmail.com>
2019-02-17 21:11:53 -0800

    Implemented forward checking heuristic

--- src/BTSolver.cpp
+++ src/BTSolver.cpp
@@ -44,7 +44,49 @@ bool BTSolver::assignmentsCheck ( void )
  */
 bool BTSolver::forwardChecking ( void )
 {
-	return false;
+	int numChanges = 0;
+	do
+	{
+		numChanges++;
+
+		// Get all of the constraints that were changed from last move
+		for (Constraint *c : network.getModifiedConstraints())
+		{
+			for (Variable *v : c->vars)
+			{
+				// Only forward check the variables(box) that have been assigned and we can eliminate from its neighbors
+				if (v->isAssigned())
+				{
+					for (Variable *neighbor : network.getNeighborsOfVariable(v))
+					{
+						// Check the domain of the neighbors and if our recently assigned variable is in there
+						if (neighbor->getDomain().contains(v->getAssignment()))
+						{
+							trail->push(neighbor);								 // Add the previous state to the trail so we can backtrack
+							neighbor->removeValueFromDomain(v->getAssignment()); // Remove the assigned variable
+							numChanges = 0;										 // Reset the counter so we have to forward check again
+						}
+					}
+				}
+			}
+		}
+	} while (numChanges == 0);
+
+	// Debugging code for checking the domain of all the variables
+	for (Variable *v : network.getVariables())
+	{
+		cout << v->toString() << endl;
+	}
+
+	// Go through the board to make sure it's consistent and there aren't any empty squares
+	for (Variable *v : network.getVariables())
+	{
+		if (v->getDomain().isEmpty())
+		{
+			return false;
+		}
+	}
+	return true;
 }
 
 /**
